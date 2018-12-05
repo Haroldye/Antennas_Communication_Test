8574afd6b417384c35dc3c3c03f85445c52d343b
Howar <Haroldye@users.noreply.github.com>
2018-12-05 16:11:08 -0500

    Add files via upload

---
+++ rf69_server_AddKeypad/rf69_server_AddKeypad.ino
@@ -0,0 +1,101 @@
+// rf69_server.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing server
+// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
+// reliability, so you should only use RH_RF69  if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf69_client
+// Demonstrates the use of AES encryption, setting the frequency and modem
+// configuration.
+// Tested on Moteino with RFM69 http://lowpowerlab.com/moteino/
+// Tested on miniWireless with RFM69 www.anarduino.com/miniwireless
+// Tested on Teensy 3.1 with RF69 on PJRC breakout board
+
+#include <SPI.h>
+#include <RH_RF69.h>
+
+// Singleton instance of the radio driver
+RH_RF69 rf69;
+//RH_RF69 rf69(15, 16); // For RF69 on PJRC breakout board with Teensy 3.1
+//RH_RF69 rf69(4, 2); // For MoteinoMEGA https://lowpowerlab.com/shop/moteinomega
+//RH_RF69 rf69(8, 7); // Adafruit Feather 32u4
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial);
+  if (!rf69.init())
+    Serial.println("init failed");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(433.0))
+    Serial.println("setFrequency failed");
+  //Set rf69 Modulation scheme (2-ary)BPSK here
+  //OOK, Whitening, Rb = 2.4kbs, Rx Bandwidth = 4.8kHz.
+  //rf69.setModemConfig(22  
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  //rf69.setTxPower(14, true);
+
+  // The encryption key has to be the same as the one in the client
+  uint8_t key[] = { 0x08, 0x08, 0x08, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  rf69.setEncryptionKey(key);
+  
+#if 0
+  // For compat with RFM69 Struct_send
+  rf69.setModemConfig(RH_RF69::GFSK_Rb250Fd250);
+  rf69.setPreambleLength(3);
+  uint8_t syncwords[] = { 0x2d, 0x64 };
+  rf69.setSyncWords(syncwords, sizeof(syncwords));
+  rf69.setEncryptionKey((uint8_t*)"thisIsEncryptKey");
+#endif
+}
+
+void loop()
+{
+  //Serial.println("got sth. ");
+  if (rf69.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf69.recv(buf, &len))
+    {
+//      RH_RF69::printBuffer("request: ", buf, len);
+      Serial.print("got request: ");
+      Serial.println((char*)buf);
+//      Serial.print("RSSI: ");
+//      Serial.println(rf69.lastRssi(), DEC);
+      
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf69.send(data, sizeof(data));
+      rf69.waitPacketSent();
+      Serial.println("Sent a reply");
+      
+      //add screen show;
+      while(1){
+        if (rf69.available())
+      {
+          Serial.print("got sth. ");
+        // Should be a message for us now   
+          uint8_t buf1[1];
+          uint8_t len1 = sizeof(buf1);
+        if (rf69.recv(buf1, &len1))
+        {
+    //      RH_RF69::printBuffer("request: ", buf, len);
+          Serial.print("got request: ");
+          Serial.println((char*)buf1);
+    //      Serial.print("RSSI: ");
+    //      Serial.println(rf69.lastRssi(), DEC);
+        }}
+      }
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+
+  }
+}
